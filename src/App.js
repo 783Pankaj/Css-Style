769381b93dc6bf3ed84ace5af4339cc51f6c4156
Pankaj Pal <munapal75917@gmail.com>
2023-07-19 17:04:32 +0530

    all css

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
 import './App.css';
-
+import CssStyle from './styles/CssStyle';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    // <div>
+    //   <h1>Hello user</h1>
+    //   <h2 style={{ color: 'red' }}>this is h2</h2>
+    //   <hr />
+    //   <div  className="App">
+
+    //   {/* <p className='paragraph'>
+    //     The Coppa Italia is an annual football cup competition established in Italy in 1922. The competition is open to all Serie A and Serie B clubs, as well as four teams from Serie C.[1]
+    //     Since the first final between Vado and Udinese in 1922, 75 finals have taken place. There have been 41 single-match finals, one of which was replayed after the initial game ended in a goalless draw.
+    //   </p>
+    //   <p className='paragraph'>
+    //     The Coppa Italia is an annual football cup competition established in Italy in 1922. The competition is open to all Serie A and Serie B clubs, as well as four teams from Serie C.[1]
+    //     Since the first final between Vado and Udinese in 1922, 75 finals have taken place. There have been 41 single-match finals, one of which was replayed after the initial game ended in a goalless draw.
+    //   </p> */}
+    //   <div id='text'>
+    //   <p >
+    //     The Coppa Italia is an annual                       football cup competition established in Italy in 1922. The competition is open to all Serie A and Serie B clubs, as well as four teams from Serie C.
+    //     Since the first final between Vado and Udinese in 1922, 75 finals have taken place. There have been 41 single-match finals, one of which was replayed after the initial game ended in a goalless draw.
+    //   </p>
+    //   </div>
+    //   <div>
+    //     <a href='www.gogle.com'>Facebook</a>
+    //   </div>
+    //   </div>
+    // </div>
+    <>
+    {/* <div id='units'>
+    <p >
+      The Coppa Italia is an annual                       football cup competition established in Italy in 1922. The competition is open to all Serie A and Serie B clubs, as well as four teams from Serie C.
+      Since the first final between Vado and Udinese in 1922, 75 finals have taken place. There have been 41 single-match finals, one of which was replayed after the initial game ended in a goalless draw.
+    </p>
     </div>
+
+    <div className='cursore'>
+      <p>
+        this is me 
+      </p>
+    </div> */}
+
+    <CssStyle />
+    </>
   );
 }
-
 export default App;
---
+++ src/styles/CssStyle.js
@@ -0,0 +1,134 @@
+import React from 'react'
+import '../styles/style.css'
+
+const CssStyle = () => {
+    return (
+        <>
+            {/* <div className='optices'>
+                <p>This is optices</p>
+            </div>
+            <div>
+                <img src='logo512.png' alt='' />
+            </div> */}
+
+            {/* <div id='div1'>
+
+            </div> */}
+
+            {/* <div id='overflow'>
+                <p>
+                In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content. Lorem ipsum may be used as a placeholder before final copy is available. It is also used to temporarily replace text in a process called greeking, which allows designers to consider the form of a webpage or publication, without the meaning of the text influencing the design.
+Lorem ipsum is typically a corrupted version of De finibus bonorum et malorum, a 1st-century BC text by the Roman statesman and philosopher Cicero, with words altered, added, and removed to make it nonsensical and improper Latin. The first two words themselves are a truncation of 'dolorem ipsum' ('pain itself').
+Versions of the Lorem ipsum text have been used in typesetting at least since the 1960s, when it was popularized by advertisements for Letraset transfer sheets.[1] Lorem ipsum was introduced to the digital world in the mid-1980s, when Aldus employed it in graphic and word-processing templates for its desktop publishing program PageMaker. Other popular word processors, including Pages and Microsoft Word, have since adopted Lorem ipsum,[2] as have many LaTeX packages,[3][4][5] web content managers such as Joomla! and WordPress, and CSS libraries such as Semantic UI.[6]
+Example text
+A common form of Lorem ipsum reads:
+Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                </p>
+            </div>
+            <div>
+                <h2>unordered list</h2>
+                <ul>
+                    <li>Item 1</li>
+                    <li>Item 2</li>
+                    <li>Item 3</li>
+                    <li>Item 4</li>
+                    <li>Item 5</li>
+                </ul>
+               <hr/>
+                <h2>ordered list</h2>
+                <ol>
+                    <li>Item 1</li>
+                    <li>Item 2</li>
+                    <li>Item 3</li>
+                    <li>Item 4</li>
+                    <li>Item 5</li>
+                </ol>
+            </div>
+            <div>
+                <a id='fun' href='www.google.com'>Google</a>
+            </div> */}
+
+
+{/* 19 jul */}
+
+            {/* <div>
+                <img src='img1.jpg' alt=''/>
+            </div> */}
+
+            {/* <div>
+                <h1>heading </h1>
+                <p>Al-Walid ibn Abd al-Malik ibn Marwan (Arabic: الوليد بن عبد الملك بن مروان, romanized: al-Walīd ibn ʿAbd al-Malik ibn Marwān; c. 674 – 23 February 715),[a] commonly known as al-Walid I (Arabic: الوليد الأول), was the sixth Umayyad caliph, ruling from October 705 until his death. He was the eldest son of his predecessor, Caliph Abd al-Malik (r. 685–705). As a prince, he led annual raids against the Byzantines from 695 to 698 and built or restored fortifications along the Syrian Desert route to Mecca. He became heir apparent in c. 705, after the death of the designated successor, Abd al-Malik's brother Abd al-Aziz ibn Marwan.
+Under al-Walid, his father's efforts to centralize government, impose a more Arabic and Islamic character on the state, and expand its borders were continued. He heavily depended on al-Hajjaj ibn Yusuf, his father's powerful viceroy over the eastern half of the caliphate. During his reign, armies commissioned by al-Hajjaj conquered Sind and Transoxiana in the east, while the troops of Musa ibn Nusayr, the governor of Ifriqiya, conquered the Maghreb and Hispania in the west, bringing the caliphate to its largest territorial extent. War spoils from the conquests enabled al-Walid to finance impressive public works, including his greatest architectural achievement, the Great Mosque of Damascus, as well as the al-Aqsa Mosque in Jerusalem and the Prophet's Mosque in Medina. He was the first caliph to institute programs for social welfare, aiding the poor and handicapped among the Muslim Arabs of Syria, who held him in high esteem.
+
+His reign was marked by domestic peace and prosperity and likely represented the peak of Umayyad power, though it is difficult to ascertain his direct role in its affairs. The balance al-Walid maintained among the elites, including the Qays and Yaman army factions, may have been his key personal achievement. On the other hand, the massive military expenditures of his rule, as well as his extravagant grants to the Umayyad princes, became a financial burden on his successors.</p>
+            </div> */}
+
+            {/* <div className='display'> 
+                This is a Div
+            </div>
+
+            <div className='display'> 
+                This is a Div
+            </div> */}
+
+            {/* <div id='parent'>
+              <div id='one'></div>
+              <div id='two'></div>
+              <div id='three'></div>
+            </div> */}
+
+            {/* <p> <img id='img1' src='img1.jpg' width={200}/>
+            Al-Walid ibn Abd al-Malik ibn Marwan (Arabic: الوليد بن عبد الملك بن مروان, romanized: al-Walīd ibn ʿAbd al-Malik ibn Marwān; c. 674 – 23 February 715),[a] commonly known as al-Walid I (Arabic: الوليد الأول), was the sixth Umayyad caliph, ruling from October 705 until his death. He was the eldest son of his predecessor, Caliph Abd al-Malik (r. 685–705).<img id='img2' src='logo192.png' width={200}/> As a prince, he led annual raids against the Byzantines from 695 to 698 and built or restored fortifications along the Syrian Desert route to Mecca. He became heir apparent in c. 705, after the death of the designated successor, Abd al-Malik's brother Abd al-Aziz ibn Marwan.
+            </p> */}
+
+            {/* <ul>
+                <li>Home</li>
+                <li>About</li>
+                <li>Team</li>
+                <li>Contacts us</li>
+                <li>Donate</li>
+            </ul> */}
+{/* 
+            <div id='wrapper'>
+                <a href=''>Click here</a>
+            </div> */}
+
+            {/* <div id='cover'>
+                <div id='content'>
+
+                </div>
+            </div> */}
+            {/* <div id='transection-div'>
+                div
+            </div> */}
+
+            
+
+            {/* Animation */}
+
+            {/* <div id='animate'></div>
+            <hr/>
+            <div id='animate2'></div> */}
+
+            {/* <div>
+                <p>
+                    <img src='img1.jpg' width={200}/>
+                    Al-Walid ibn Abd al-Malik ibn Marwan (Arabic: الوليد بن عبد الملك بن مروان, romanized: al-Walīd ibn ʿAbd al-Malik ibn Marwān; c. 674 – 23 February 715),[a] commonly known as al-Walid I (Arabic: الوليد الأول), was the sixth Umayyad caliph, ruling from October 705 until his death. He was the eldest son of his predecessor, Caliph Abd al-Malik (r. 685–705). As a prince, he led annual raids against the Byzantines from 695 to 698 and built or restored fortifications along the Syrian Desert route to Mecca. He became heir apparent in c. 705, after the death of the designated successor, Abd al-Malik's brother Abd al-Aziz ibn Marwan.   
+                </p>
+            </div> */}
+
+            {/* <div>
+
+                <img src='img1.jpg' width={200}/>
+                <p>
+                Al-Walid ibn Abd al-Malik ibn Marwan (Arabic: الوليد بن عبد الملك بن مروان, romanized: al-Walīd ibn ʿAbd al-Malik ibn Marwān; c. 674 – 23 February 715),[a] commonly known as al-Walid I (Arabic: الوليد الأول), was the sixth Umayyad caliph, ruling from October 705 until his death. He was the eldest son of his predecessor, Caliph Abd al-Malik (r. 685–705). As a prince, he led annual raids against the Byzantines from 695 to 698 and built or restored fortifications along the Syrian Desert route to Mecca. He became heir apparent in c. 705, after the death of the designated successor, Abd al-Malik's brother Abd al-Aziz ibn Marwan.   
+                </p>
+            </div> */}
+            <div id='div1'>Div 1</div>
+            <div id='div2'>Div 2</div>
+        </>
+
+    )
+}
+
+export default CssStyle
